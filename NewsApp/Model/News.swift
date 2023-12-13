68fad64ea493d31d05811d6095ad3f57fd72b308
Yunus <skysoft.founder@gmail.com>
2023-12-13 23:43:05 +0300

    second commit

--- NewsApp/Model/News.swift
+++ NewsApp/Model/News.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+
+
+struct News: Decodable {
+    let title : String
+    let story : String
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- NewsApp/Service/WebService.swift
+++ NewsApp/Service/WebService.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+
+class WebService {
+    
+    func haberIndir(url: URL, completion: @escaping ([News]?) -> ()) {
+        URLSession.shared.dataTask(with: url) { (data, response, error)  in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            } else if let data = data {
+                let haberlerDizisi = try? JSONDecoder().decode([News].self, from: data)
+                if let haberlerDizisi = haberlerDizisi {
+                    completion(haberlerDizisi)
+                }
+                
+            }
+        }.resume()
+        
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
